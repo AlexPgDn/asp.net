176db2c5022608858cb2497bc6408d7ddbaf17b5
Alex_PgDn <3alexn3@gmail.com>
2024-09-18 19:57:24 +0300

    delete+

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ const App = () => {
         setContacts([...contacts,item]);
   }
   const deleteContact = (id) => {
+    axios.delete(`${baseApiUrl}/contacts/${id}`)
     setContacts(contacts.filter(item => item.id !== id));
   }
 
